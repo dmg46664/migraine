a608842c294ef7e554e4e512590236e477319428
Aidan Nagorcka-Smith <aidanns@gmail.com>
2013-09-28 22:42:01 +1000

    Added a Java demo that makes sense. Cleaned up the project by removing sub-projects that aren't needed.

--- core/src/main/java/coza/mambo/migraine/core/Migraine.java
+++ core/src/main/java/coza/mambo/migraine/core/Migraine.java
@@ -1,89 +1,85 @@
 package coza.mambo.migraine.core;
 
-import static playn.core.PlayN.*;
-
 import playn.core.*;
 import playn.core.util.Clock;
+import tripleplay.game.ScreenStack;
+import tripleplay.game.UIAnimScreen;
 import tripleplay.ui.*;
 import tripleplay.ui.layout.AxisLayout;
 
+/**
+ * A simple example of using MigLayout in a TriplePlay application.
+ * @author Aidan Nagorcka-Smith (aidanns@gmail.com)
+ */
 public class Migraine extends Game.Default {
-
-	private Interface iface;
-	private final static int UPDATE_RATE = 33;
-	private final Clock.Source clock = new Clock.Source(UPDATE_RATE);
-	private Route route;
 	
-	enum Route {
-		TRIPLEPLAY, MIGLAYOUT;
-	};
+	/**
+	 * Screen to display.
+	 */
+	private class DemoScreen extends UIAnimScreen {
+		
+		// The root of this screens UI.
+		private Root _root;
+		
+		@Override
+		public void wasAdded() {
+			super.wasAdded();
+			_root = iface.createRoot(AxisLayout.vertical().gap(0).offStretch(), SimpleStyles.newSheet(), layer);
+			_root.setSize(width(), height());
+			
+			// Create a MigGroup with two columns and two rows.
+			// The columns both take up 50% of the available width.
+			// The top row takes 10% of the available height, while the bottom takes 90%.
+			// The fill constraint makes this layout fill its container.
+			MigGroup group = new MigGroup(new MigLayout("fill", "[50%][50%]", "[10%][90%]"));
 
+			// Create four groups and add them to a specific cell in the grid of the MigLayout.
+			// The "grow" constraint makes these elements fill their cells.
+			Group topLeft = new Group(AxisLayout.vertical());
+			topLeft.setStyles(Style.BACKGROUND.is(Background.solid(0xFFFF0000)));
+			group.add(topLeft, "cell 0 0, grow");
+			
+			Group topRight = new Group(AxisLayout.vertical());
+			topRight.setStyles(Style.BACKGROUND.is(Background.solid(0xFF00FF00)));
+			group.add(topRight, "cell 0 1, grow");
+			
+			Group bottomLeft = new Group(AxisLayout.vertical());
+			bottomLeft.setStyles(Style.BACKGROUND.is(Background.solid(0xFF0000FF)));
+			group.add(bottomLeft, "cell 1 0, grow");
+			
+			Group bottomRight = new Group(AxisLayout.vertical());
+			bottomRight.setStyles(Style.BACKGROUND.is(Background.solid(0xFF000000)));
+			group.add(bottomRight, "cell 1 1, grow");
+			
+			_root.add(group.setConstraint(AxisLayout.stretched()));
+		}
+	}
+
+	private final static int UPDATE_RATE = 50;
+	private final Clock.Source _clock = new Clock.Source(UPDATE_RATE);
+	
+	// Use a screenstack to display our custom screen.
+	private final ScreenStack _screens = new ScreenStack();
+	
 	public Migraine() {
-		super(UPDATE_RATE); // call update every 33ms (30 times per second)
+		super(UPDATE_RATE);
 	}
 
 	@Override
 	public void init() {
-		// create and add background image layer
-		Image bgImage = assets().getImage("images/bg.png");
-		ImageLayer bgLayer = graphics().createImageLayer(bgImage);
-		graphics().rootLayer().add(bgLayer);
-
-		GroupLayer layer = graphics().createGroupLayer();
-		iface = new Interface(); //used to take in delegate as argument
-		Stylesheet sheet = SimpleStyles.newSheet();
-		final Root root = iface.createRoot(AxisLayout.vertical().gap(5), sheet);
-		layer.add(root.layer);
-		root.addStyles(Style.HALIGN.left);
-
-		// Change this enum to see the Miglayout vs TriplePlay examples.
-		route = Route.MIGLAYOUT;
-
-		if (route == Route.MIGLAYOUT) {
-			MigGroup migraineButtons;
-			MigLayout layout = new MigLayout("", "[]40[]40[]");
-			root.add(migraineButtons = new MigGroup(layout));
-			final String e = "";
-			migraineButtons.add(new Button("A"), e);
-			migraineButtons.add(new Button("BC"), e);
-			migraineButtons.add(new Button("C"), "wrap");
-			migraineButtons.add(new Button("DEF"), e);
-			migraineButtons.add(new Button("E"), e);
-		} else /* TriplePlay */ {
-			Group group = new Group(new AxisLayout.Horizontal());
-			root.add(group);
-			group.add(new Button("A"));
-			group.add(new Button("B"));
-			group.add(new Button("C"));
-		}
-
-		PlayN.graphics().rootLayer().add(layer);
-		root.pack();
-	}
-
-	public void setStyle(Button button, int bgColor) {
-		int ulColor = 0xFFEEEEEE, brColor = 0xFFAAAAAA;
-		Background butBg = Background.roundRect(bgColor, 5, ulColor, 2).inset(5, 6, 1, 6);
-		Background butSelBg = Background.roundRect(bgColor, 5, brColor, 2).inset(6, 5, 1, 7);
-		button.addStyles(Styles.make(Style.BACKGROUND.is(butBg))
-				.addSelected(Style.BACKGROUND.is(butSelBg))
-				.add(Style.COLOR.is(0xFFFFFFFF))
-				.add(Style.FONT.is(PlayN.graphics().createFont("Aharoni", Font.Style.PLAIN, 75)))
-				.add(Style.TEXT_EFFECT.shadow)
-				.add(Style.SHADOW.is(0x20000000)).add(Style.SHADOW_X.is(1f)).add(Style.SHADOW_Y.is(1f))
-				.add(Style.VALIGN.center)
-				.add(Button.DEBOUNCE_DELAY.is(200)));
+		// Push the custom screen on to the screenstack to display it.
+		_screens.push(new DemoScreen());
 	}
 
 	@Override
 	public void update(int delta) {
-		clock.update(delta);
-		iface.update(delta);
+		_clock.update(delta);
+		_screens.update(delta);
 	}
 
 	@Override
 	public void paint(float alpha) {
-		clock.paint(alpha);
-		iface.paint(clock);
+		_clock.paint(alpha);
+		_screens.paint(_clock);
 	}
 }
--- java/src/main/java/coza/mambo/migraine/java/MigraineJava.java
+++ java/src/main/java/coza/mambo/migraine/java/MigraineJava.java
@@ -1,20 +1,14 @@
 package coza.mambo.migraine.java;
 
-import coza.mambo.migraine.core._;
 import playn.core.PlayN;
 import playn.java.JavaPlatform;
 
 import coza.mambo.migraine.core.Migraine;
 
 public class MigraineJava {
-
-  public static void main(String[] args) {
-	  //for debugging
-	  _.initialize();
-
-    JavaPlatform.Config config = new JavaPlatform.Config();
-    // use config to customize the Java platform, if needed
-    JavaPlatform.register(config);
-    PlayN.run(new Migraine());
-  }
+	public static void main(String[] args) {
+		JavaPlatform.Config config = new JavaPlatform.Config();
+		JavaPlatform.register(config);
+		PlayN.run(new Migraine());
+	}
 }
